ed15cdd2fdd525a04d5799f97debbfc8bd4c41cb
petez <pete.zsolt@codeyard.eu>
2017-07-13 21:43:03 +0200

    ✨ Create reactively rows

--- TestRxDataSource/People/PeopleViewController.swift
+++ TestRxDataSource/People/PeopleViewController.swift
@@ -7,89 +7,79 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
 
 class PeopleViewController: UITableViewController {
-
+    
+    let disposeBag = DisposeBag()
+    let developers: [[PeopleViewModel]] = [
+        [
+            PeopleViewModel(userName: "Petez", age: 21),
+            PeopleViewModel(userName: "EGabor", age: 22),
+            PeopleViewModel(userName: "iUjlaki", age: 27)
+        ],
+        [
+            PeopleViewModel(userName: "Boros", age: 23),
+            PeopleViewModel(userName: "Gizmo", age: 30),
+            PeopleViewModel(userName: "Lali", age: 27)
+        ],
+        [
+            PeopleViewModel(userName: "Papó", age: 27)
+        ]
+    ]
+    var sections: Variable<[PeopleSection]> = Variable([])
+    
+    var dataSource: RxTableViewSectionedReloadDataSource<PeopleSection> = RxTableViewSectionedReloadDataSource<PeopleSection>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.initDevelopers()
+        self.initTableView()
+        self.configureCell()
+        self.bindSections()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func initDevelopers(){
+        self.sections.value = [
+            PeopleSection(header: "iOS Team", items: developers[0]),
+            PeopleSection(header: "Android Team", items: developers[1]),
+            PeopleSection(header: "Web Team", items: developers[2])
+        ]
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    func initTableView(){
+        tableView.delegate = nil
+        tableView.dataSource = nil
+        tableView.rx.setDelegate(self)
+            .addDisposableTo(disposeBag)
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    func configureCell(){
+        dataSource.configureCell = { dataSource, tableView, indexPath, item in
+            let cell: PeopleCell = tableView.dequeueReusableCell(withIdentifier: Constants.Cells.PeopleCell) as! PeopleCell
+            cell.bindTo(viewModel: item)
+            return cell
+        }
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    func bindSections(){
+        sections.asObservable()
+            .bind(to: tableView.rx.items(dataSource: dataSource))
+            .addDisposableTo(disposeBag)
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    @IBAction func addDeveloper(_ sender: Any) {
+        self.addDeveloper()
+        guard let addButton: UIButton = sender as? UIButton else {
+            return
+        }
+        addButton.isEnabled = false
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func addDeveloper(){
+        self.sections.value[2].items.append(PeopleViewModel(userName: "Saca", age: 21))
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
