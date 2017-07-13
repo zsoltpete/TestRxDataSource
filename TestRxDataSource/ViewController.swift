296f34b040ba4aaf230ce71a92c9e32f40366853
petez <pete.zsolt@codeyard.eu>
2017-07-13 19:45:32 +0200

    Initial Commit

---
+++ TestRxDataSource/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestRxDataSource
+//
+//  Created by Zsolt Pete on 2017. 07. 13..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
