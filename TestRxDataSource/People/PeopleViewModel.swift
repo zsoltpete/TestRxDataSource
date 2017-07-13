206af2fee3a09d43d5e68f8f0c1b2502c2e3f996
petez <pete.zsolt@codeyard.eu>
2017-07-13 21:42:31 +0200

    ✨ View model implemted

--- TestRxDataSource/People/PeopleViewModel.swift
+++ TestRxDataSource/People/PeopleViewModel.swift
@@ -7,7 +7,28 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class PeopleViewModel: NSObject {
+    
+    var userName: Variable<String> = Variable("")
+    var age: Variable<Int> = Variable(0)
+    
+    init(userName: String, age: Int) {
+        self.userName.value = userName
+        self.age.value = age
+        super.init()
+    }
+    
+    public static func ==(lhs: PeopleViewModel, rhs: PeopleViewModel) -> Bool {
+        return true
+    }
+    
+    override init() {
+        self.userName.value = ""
+        self.age.value = 0
+        super.init()
+    }
 
 }
