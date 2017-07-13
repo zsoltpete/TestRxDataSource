93df653fd3e15f73b2f1d7584990e777bd82fc42
petez <pete.zsolt@codeyard.eu>
2017-07-13 21:38:05 +0200

    first commit

---
+++ TestRxDataSource/People/PeopleCell.swift
@@ -0,0 +1,24 @@
+//
+//  PeopleCell.swift
+//  TestRxDataSource
+//
+//  Created by Zsolt Pete on 2017. 07. 13..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class PeopleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TestRxDataSource/Utilities/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  TestRxDataSource
+//
+//  Created by Zsolt Pete on 2017. 07. 13..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import Foundation
