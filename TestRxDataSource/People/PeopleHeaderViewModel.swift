e52769921619c97cf766fcf0b2fdafd2d87e1673
petez <pete.zsolt@codeyard.eu>
2017-07-13 22:34:27 +0200

    ➕ Added new classes

---
+++ TestRxDataSource/People/PeopleHeaderViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PeopleHeaderViewModel.swift
+//  TestRxDataSource
+//
+//  Created by Zsolt Pete on 2017. 07. 13..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class PeopleHeaderViewModel: NSObject {
+
+}
